ba77b32cc992bea53e1291cefe55807d20c372cd
Melya <melenko55@gmail.com>
2021-09-14 13:12:34 +0300

    Fixed bugs with input focus, changed icons and added bg-images

--- script.js
+++ script.js
@@ -1,45 +1,70 @@
-const wrapper = document.querySelector(".inner-container-img-block");
-const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
-  "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"
-];
-const daysInWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-wrapper.addEventListener('click', (e) => {
-    if (e.target.tagName == "IMG") {
-        document.querySelector(".page").style.backgroundImage = `url("${e.target.src}")`;
-    };
-});
+document.addEventListener("DOMContentLoaded", () => {
+    //bg-change func
+    const wrapper = document.querySelector(".inner-container-img-block"),
+    bgArray = wrapper.querySelectorAll(".bg-image"),
+    page = document.querySelector(".page");
 
-
-
-const date = document.querySelector(".date"),
-      day = date.querySelector(".day"),
-      month = date.querySelector(".month"),
-      time = date.querySelector(".time");
-
-function formatDateCurrentTime(dataNow) {
-    let hours = dataNow.getHours();
-    let minutes = dataNow.getMinutes();
-    let sec = dataNow.getSeconds();
-
-    if (hours < 10) {
-        hours = `0${hours}`;
-    }
-    if (minutes < 10) {
-        minutes = `0${minutes}`;
-    }
-    if (sec < 10) {
-        sec = `0${sec}`;
+    const clearSelected = () => {
+        bgArray.forEach(elem => elem.classList.remove("selected"));
     }
 
+    wrapper.addEventListener('click', (e) => {
+        if (e.target.tagName == "IMG") {
+            clearSelected();
+            e.target.parentElement.classList.add("selected");
+            page.style.backgroundImage = `url("${e.target.src}")`;
+        };
+    });
 
-    return `${hours}:${minutes}:${sec}`;
-}
 
-const setDate = () => {
-    const dataNow = new Date();
-    day.innerHTML = `${daysInWeek[dataNow.getDay()]} ${dataNow.getDate()}`;
-    month.innerHTML = `${monthNames[dataNow.getMonth()]}`;
-    time.innerHTML = formatDateCurrentTime(dataNow);
-}
-      
-const timeInterval = setInterval(setDate, 1000);
+    //timer
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"
+    ];
+
+    const daysInWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+
+    const date = document.querySelector(".date"),
+    day = date.querySelector(".day"),
+    month = date.querySelector(".month"),
+    time = date.querySelector(".time");
+
+    function formatDateCurrentTime(dataNow) {
+        let hours = dataNow.getHours();
+        let minutes = dataNow.getMinutes();
+        let sec = dataNow.getSeconds();
+
+        if (hours < 10) {
+            hours = `0${hours}`;
+        }
+        if (minutes < 10) {
+            minutes = `0${minutes}`;
+        }
+        if (sec < 10) {
+            sec = `0${sec}`;
+        }
+
+
+        return `${hours}:${minutes}:${sec}`;
+    }
+
+    const setDate = () => {
+        const dataNow = new Date();
+        day.innerHTML = `${daysInWeek[dataNow.getDay()]} ${dataNow.getDate()}`;
+        month.innerHTML = `${monthNames[dataNow.getMonth()]}`;
+        time.innerHTML = formatDateCurrentTime(dataNow);
+    }
+
+    const timeInterval = setInterval(setDate, 1000);
+
+
+    //settings
+    const settings =  document.querySelector(`[data-setting="setting"]`),
+          window = document.querySelector(".window");
+    
+    settings.addEventListener('click', () => {
+        window.classList.toggle("hide");
+    });
+})
+
